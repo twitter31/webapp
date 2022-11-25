e863d7571bb822d82ded4b8e1213af2daa15ac84
Emy A <emmerencea@yahoo.com>
2022-11-25 23:53:08 +0000

    tested and approved by the client QA team

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,9 @@ Deploy python apps
 deploy in the dev env
 deployment5 master
 deployment4
+<<<<<<< HEAD
+=======
+deploy to staging env
+echo good job
+Tested and accepted
+>>>>>>> 2aac72f (tested and approved by the client QA team)
